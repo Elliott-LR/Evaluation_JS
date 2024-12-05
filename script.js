dae37d0868148edae636caa3ab2882fc1799c4f5
Elliott-Latoile <elliott.lanza@gmail.com>
2024-12-05 16:26:42 +0100

    JS presque fini

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ fetch("sneakers.json")
     })
     .then((données) => {
         console.log(données);
-        // "recettes" est un tableau contenant plusieurs recettes
-        // Je boucle dessus et pour chaque recette :
+        // "données" est un tableau contenant plusieurs données
+        // Je boucle dessus et pour chaque données :
         // Je l'affiche dans ma page web
         données.produits.forEach((produits) => {
-            afficher(produits);
+            afficherProduits(produits);
+        });
+        données.avantagesClients.forEach((avantageClient) => {
+            afficherAvantages(avantageClient);
+        });
+        données.services.forEach((services) => {
+            afficherServices(services);
+        });
+        données.temoignages.forEach((temoignages) => {
+            afficherTemoignages(temoignages);
         });
     });
 
 // Fonction pour afficher les produits dans la page
-function afficher(produits) {
+function afficherProduits(produits) {
     // Extraction des données du produit
     let nom = produits.nom; // Nom du produit
     let description = produits.description; // description du produit
@@ -24,15 +33,79 @@ function afficher(produits) {
 
     // Construction de la carte HTML pour un produit
     let carte = `
-        <div class="produits">
             <div class="produit">
                 <img src="assets/${image}" alt="Photo Sneaker">
+                <h3>${nom}</h3>
+                <p>${description}</p>
+            </div>
+
+    `;
+
+    // Récupère le conteneur des cartes dans la page HTML
+    let containerCards = document.querySelector(".produits");
+    containerCards.innerHTML += carte;
+}
+
+// Fonction pour afficher les produits dans la page
+function afficherAvantages(avantagesClients) {
+    // Extraction des données du produit
+    let image = avantagesClients.image; // image du produit
+    let avantage = avantagesClients.avantage; // avantage du produit
+
+    // Construction de la carte HTML pour un produit
+    let carte = `
+            <div class="client">
+                <img src="assets/${image}" alt="logo des avantages">
+                <p>${avantage}</p>
+            </div>
+    `;
+
+    // Récupère le conteneur des cartes dans la page HTML
+    let containerCards = document.querySelector(".clients");
+    containerCards.innerHTML += carte;
+}
+
+// Fonction pour afficher les services dans la page
+function afficherServices(services) {
+    // Extraction des données du service
+    let nom = services.nom; // Nom du service
+    let description = services.description; // description du service
+
+    // Construction de la carte HTML pour un service
+    let carte = `
+            <div class="service">
                 <h3>${nom}</h3>
                 <p>${description}</p>
             </div>
     `;
 
     // Récupère le conteneur des cartes dans la page HTML
-    let containerCards = document.querySelector(".produits");
+    let containerCards = document.querySelector(".services");
     containerCards.innerHTML += carte;
+}
+
+// Fonction pour afficher les temoignages dans la page
+function afficherTemoignages(temoignages) {
+    // Extraction des données du temoignages
+    let prenom = temoignages.prenom; // Nom du temoignages
+    let typeExperience = temoignages.typeExperience; // experience du temoignages
+    let commentaires = temoignages.commentaire; // commentaire du temoignages
+    let note = temoignages.note; // note du temoignages
+
+    // Construction de la carte HTML pour un produit
+    let carte = `
+            <div class="temoin">
+                <h3>${prenom}</h3>
+                <p>${typeExperience}</p>
+                <p>${commentaires}</p>
+                <p>${etoile(note)}</p>
+            </div>
+    `;
+
+    // Récupère le conteneur des cartes dans la page HTML
+    let containerCards = document.querySelector(".temoins");
+    containerCards.innerHTML += carte;
+}
+function etoile(note) {
+    return "★".repeat(note) + "☆".repeat(5 - note)
 }
